de917c164b86893932bde1fe901d440c5a9a9f79
yiningliu1 <145304243+yiningliu1@users.noreply.github.com>
2024-01-12 23:07:02 -0500

    organized files better

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const connectDB = require('./config/dbConn');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const connectDB = require("./backend/config/dbConn");
 const PORT = process.env.PORT || 3500;
 
 // connect to MongoDB
@@ -15,9 +15,9 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 
-app.use('/tweet', require('./routes/tweet'));
+app.use("/tweet", require("./backend/routes/tweet"));
 
-mongoose.connection.once('open', () => {
-  console.log('Connected to MongoDB');
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-})
+});
